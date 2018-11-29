94a7f424c2ff52774e74e2ddf39b98b9d01fd269
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-11-28 23:16:14 -0700

    Initial pages commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+ module.exports = {
+   devtool: 'inline-source-map',
+   entry: {
+     main: "./lib/index.js",
+   },
+   output: {
+     path: __dirname,
+     filename: "[name].bundle.js"
+   },
+   mode: 'development',
+   module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: "style!css" },
+      { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] },
+    ]
+   },
+   resolve: {
+     extensions: ['.js', '.json', '.css']
+   }
+ };
